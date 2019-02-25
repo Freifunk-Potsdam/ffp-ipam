375002c0f4f286efa79a3c224ed1f279ae7a07e4
Justin Humm <justin.humm@posteo.de>
2019-02-25 15:50:21 +0100

    first steps in catching db errors

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,17 @@ pub mod schema;
 
 use self::diesel::prelude::*;
 use auth::Token;
-use diesel::pg::PgConnection;
+use diesel::result::{DatabaseErrorKind, Error};
 use ipnetwork::IpNetwork;
-use rocket::http::{Status, ContentType};
+use rocket::http::{ContentType, Status};
+use rocket::request::Request;
+use rocket::response;
+use rocket::response::{Responder, Response};
 use rocket_contrib::databases::diesel as rocket_diesel;
 use rocket_contrib::json::{Json, JsonValue};
 use schema::ip4s;
 use schema::ip4s::dsl::*;
 use std::collections::HashMap;
-use rocket::Catcher;
-use rocket::response;
-use rocket::response::{Response, Responder};
-use rocket::request::Request;
 
 #[database("pg_db")]
 struct DbConn(rocket_diesel::PgConnection);
@@ -46,16 +45,13 @@ fn get(_t: Token, conn: DbConn) -> JsonValue {
     let res = ip4s.load::<Ip4>(&conn.0).unwrap();
     // Make the primary key 'ip' the key of a JSON dict
     let res_folded = res.iter().fold(HashMap::new(), |mut acc, x| {
-        acc.insert(
-            x.ip,
-            { 
-                let mut res = HashMap::new();
-                res.insert("node_name", x.node_name.clone());
-                res.insert("location", x.location.clone());
-                res.insert("contact", x.contact.clone());
-                res
-            },
-        );
+        acc.insert(x.ip, {
+            let mut res = HashMap::new();
+            res.insert("node_name", x.node_name.clone());
+            res.insert("location", x.location.clone());
+            res.insert("contact", x.contact.clone());
+            res
+        });
         acc
     });
     JsonValue(serde_json::to_value(&res_folded).unwrap())
@@ -64,7 +60,7 @@ fn get(_t: Token, conn: DbConn) -> JsonValue {
 #[derive(Debug)]
 struct ApiResponse {
     json: JsonValue,
-    status: Status
+    status: Status,
 }
 
 impl<'r> Responder<'r> for ApiResponse {
@@ -74,7 +70,6 @@ impl<'r> Responder<'r> for ApiResponse {
             .header(ContentType::JSON)
             .ok()
     }
-
 }
 
 #[post("/register", format = "application/json", data = "<msg>")]
@@ -82,14 +77,24 @@ fn put(_t: Token, conn: DbConn, msg: Json<Ip4>) -> ApiResponse {
     let ip4: Ip4 = msg.into_inner();
     println!("{:?}", ip4);
     match ip4 {
-        // Ip4 {ip: ipnetwork::Ipv4Network {..}, ..} => json!({"error": {"description": "Ip has wrong format"}}),
-        ip4 => {
-            diesel::insert_into(ip4s)
-                .values(ip4)
-                .execute(&conn.0)
-                .unwrap();
-            ApiResponse { json: json!({"status": "success"}), status: Status::NotFound }
-        }
+        ip4 => match diesel::insert_into(ip4s).values(ip4).execute(&conn.0) {
+            Ok(_) => ApiResponse {
+                json: json!({"status": "success"}),
+                status: Status::Ok,
+            },
+            Err(Error::DatabaseError(DatabaseErrorKind::UniqueViolation, _)) => ApiResponse {
+                json: json!({"error": "IP address already taken. Please choose a different one."}),
+                status: Status::UnprocessableEntity,
+            },
+            Err(err) => ApiResponse {
+                json: json!({
+                    "error": json!({
+                        "error": format!("Halp! An unhandled DB error happened: {:?}", err)
+                    })
+                }),
+                status: Status::InternalServerError,
+            },
+        },
     }
 }
 
